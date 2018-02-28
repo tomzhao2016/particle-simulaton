b783fea747900021700a6475b72ded71d7304a09
QingyangZhao <276676925@qq.com>
2018-02-28 02:21:34 -0800

    change size intogcd Debug

--- mpi_helper.cpp
+++ mpi_helper.cpp
@@ -268,22 +268,15 @@ void init_local_bins(bin_t* local_bins, particle_t* local_particles,int local_si
 		int local_col = glob2loc_col(global_col, idx_col,  num_proc_y, num_bin[1]);
 		std::cout<<rank<<" finished glob2loc local_row:"<<local_row<<std::endl;
 		//std::cout<<"I am processor "<<rank<<" "<<" I am particle "<<offsets[rank] + idx<<" with local_row and local_col"<<local_row<<" "<<local_col<<std::endl;
-		// Debug
-		// if(local_row < 0 || local_col < 0 && rank == 9){
-		// 	int *proc_temp = get_procs(local_particles[idx].x, local_particles[idx].y, num_proc_x, num_proc_y);
-			
-		// 	for (int temp = 0; temp < 9 ;temp++){
-		// 		std::cout<<"This particle is in "<<proc_temp[temp]<<std::endl;
-		// 	}
-		// 	std::cout<<"This particle x is "<<local_particles[idx].x<<std::endl;
-		// 	std::cout<<"This particle y is "<<local_particles[idx].y<<std::endl;
-		// 	std::cout<<"This size is "<<get_size()<<std::endl;
-		// }
-		//
-		// hard code
-		//
-
+	
 		int cur_bin = local_col * local_bin_size[0] + local_row;
+
+		// Debug
+		if( rank == 1){
+			std::cout<<"This particle x is "<<local_particles[idx].x<<std::endl;
+			std::cout<<"This particle y is "<<local_particles[idx].y<<std::endl;
+			std::cout<<"This cur_bin is "<<cur_bin<<std::endl;
+		}
 		// 
 		// insert particle into bins
 		//
