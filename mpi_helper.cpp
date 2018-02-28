b393ee3b01230ae7b807450171c0a0ecdc9ea98c
QingyangZhao <276676925@qq.com>
2018-02-28 00:21:17 -0800

    see if particles have error

--- mpi_helper.cpp
+++ mpi_helper.cpp
@@ -253,26 +253,31 @@ void init_local_bins(bin_t* local_bins, particle_t* local_particles,int local_si
 		int local_row = glob2loc_row(global_row, idx_row, num_proc_x, num_bin[0]);
 		int local_col = glob2loc_col(global_col, idx_col,  num_proc_y, num_bin[1]);
 		//std::cout<<"I am processor "<<rank<<" "<<" I am particle "<<offsets[rank] + idx<<" with local_row and local_col"<<local_row<<" "<<local_col<<std::endl;
-		if(local_row < 0 || local_col<0 && rank == 9){
-			int *proc_temp = get_procs(local_particles[idx].x, local_particles[idx].y, num_proc_x, num_proc_y);
-			std::cout<<"I am "<<rank<<" "<<std::endl;
-			for (int temp = 0; temp < 9 ;temp++){
-				std::cout<<"This particle is in "<<proc_temp[temp]<<std::endl;
-			}
-			std::cout<<"This particle x is "<<local_particles[idx].x<<std::endl;
-			std::cout<<"This particle y is "<<local_particles[idx].y<<std::endl;
-			std::cout<<"This size is "<<get_size()<<std::endl;
+		// Debug
+		// if(local_row < 0 || local_col < 0 && rank == 9){
+		// 	int *proc_temp = get_procs(local_particles[idx].x, local_particles[idx].y, num_proc_x, num_proc_y);
+		// 	std::cout<<"I am "<<rank<<" "<<std::endl;
+		// 	for (int temp = 0; temp < 9 ;temp++){
+		// 		std::cout<<"This particle is in "<<proc_temp[temp]<<std::endl;
+		// 	}
+		// 	std::cout<<"This particle x is "<<local_particles[idx].x<<std::endl;
+		// 	std::cout<<"This particle y is "<<local_particles[idx].y<<std::endl;
+		// 	std::cout<<"This size is "<<get_size()<<std::endl;
+		// }
+		//
+		// hard code
+		//
+		if(local_row > 0 || local_col > 0){
+			int cur_bin = local_col * local_bin_size[0] + local_row;
+			// 
+			// insert particle into bins
+			//
+			local_bins[cur_bin].native_particle.insert({idx ,local_particles[idx]});
 		}
-
 		//
 		// bin idx in 1D array
 		//
-		int cur_bin = local_col * local_bin_size[0] + local_row;
-		// std::cout<<"I am processor "<<rank<<" "<<"cur_bin "<<cur_bin<<" My local_bins_size is "<<local_bin_size[0]*local_bin_size[1]<<std::endl;
-		// 
-		// insert particle into bins
-		//
-		local_bins[cur_bin].native_particle.insert({idx ,local_particles[idx]});
+		
 
 	}
 	
