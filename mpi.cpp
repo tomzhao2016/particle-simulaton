aa76547beecdbc595a412d55b4e7c185831c7379
QingyangZhao <276676925@qq.com>
2018-03-01 01:57:59 -0800

    add update_local_bin test

--- mpi.cpp
+++ mpi.cpp
@@ -353,7 +353,7 @@ int main( int argc, char **argv )
         // TEST - Before moving
         for(int idx = 0; idx < local_bin_row*local_bin_col ; idx++){
             if (local_bins[idx].flag!=2){
-                for(std::map<double, particle_t>::iterator it_p = local_bins[idx].native_particle.begin; it_p != local_bins[idx].native_particle.end(); ++it_p){
+                for(std::map<double, particle_t>::iterator it_p = local_bins[idx].native_particle.begin(); it_p != local_bins[idx].native_particle.end(); ++it_p){
                     if (it_p->first == 0.0){
                         //  
                         // store map of particles in this bin
@@ -413,7 +413,7 @@ int main( int argc, char **argv )
         // TEST: before moving    
         // find first second and third particles
         for(int idx = 0; idx < local_bin_row*local_bin_col ; idx++){
-            for(std::map<double, particle_t>::iterator it_p = local_bins[idx].native_particle.begin; it_p != local_bins[idx].native_particle.end(); ++it_p){
+            for(std::map<double, particle_t>::iterator it_p = local_bins[idx].native_particle.begin(); it_p != local_bins[idx].native_particle.end(); ++it_p){
                 if (it_p->first == 0.0){
                     //  
                     // store map of particles in this bin
