9c8a4ac815c437ea207e273bee9bf7de4a2dac6b
QingyangZhao <276676925@qq.com>
2018-02-28 19:42:27 -0800

    add more comments

--- mpi_helper.h
+++ mpi_helper.h
@@ -10,15 +10,8 @@
 
 
 
-// typedef struct{
-// 	std::map<int, mbin_t> native_bins;
-// 	std::map<int, mbin_t> neighbor_bins;
-// 	std::map<int, mbin_t> edge_bins;
-// } processor_t;
-
 //void get_dest_bin(int curpos, int newpos);          
 
-//void init_mbins(mbin_t mbins, int n, particle_t* particles);
 
 int get_proc_x(double pos_x, int num_proc_x);
 int get_proc_y(double pos_y, int num_proc_y);
@@ -28,5 +21,6 @@ int* get_bin_size(int num_proc_x, int num_proc_y, int rank, int bin_len);
 int glob2loc_row(int global_row, int idx_row, int num_proc_x, int num_bin_row);
 int glob2loc_col(int global_col, int idx_col, int num_proc_y, int num_bin_col);
 void init_local_bins(bin_t* local_bins, particle_t* local_particles,int local_size, int *local_bin_size, int num_proc_x, int num_proc_y, int rank, int bin_len);
+void find_local_neighbors(bin_t *bins, int cur_bin, int len_row, int len_col);
 
 
