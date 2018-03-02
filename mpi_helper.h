becc70092391b2312a6f9271eeb8beb007baadd1
lzyaml <zhenyuanliu@zhenyuanlius-MacBook-Pro.local>
2018-03-02 05:09:12 -0800

    test

--- mpi_helper.h
+++ mpi_helper.h
@@ -20,8 +20,10 @@ int* get_procs(double pos_x, double pos_y, int num_proc_x, int num_proc_y);
 int* get_bin_size(int num_proc_x, int num_proc_y, int rank, int bin_len);
 int glob2loc_row(int global_row, int idx_row, int num_proc_x, int num_bin_row);
 int glob2loc_col(int global_col, int idx_col, int num_proc_y, int num_bin_col);
-void init_local_bins(bin_t* local_bins, particle_t* local_particles,int local_size, int *local_bin_size, int num_proc_x, int num_proc_y, int rank, int bin_len);
+void init_local_bins(bin_t* local_bins, particle_t* local_particles,int local_size, int *local_bin_size, 
+	int num_proc_x, int num_proc_y, int rank, int bin_len);
 void find_local_neighbors(bin_t *bins, int cur_bin, int len_row, int len_col);
 
-// void clean_local_bins(bin_t *local_bins, int local_bin_size);
-// void update_local_bins(bin_t *local_bins, std::map<double,partcle_t>local_particles_native, int local_size_native);
+void clean_local_bins(bin_t *local_bins, int local_bin_size);
+int update_local_bins(bin_t *local_bins, std::map<double,particle_t>local_particles_native,
+	int *local_bin_size, int num_proc_x, int num_proc_y, int rank, int bin_len);
