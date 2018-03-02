d310cdf8786ce375094ae6fe784af25dbd3671dd
Mudit Gupta <mudit@berkeley.edu>
2018-03-02 01:23:12 -0800

    Adding print statement

--- mpi.cpp
+++ mpi.cpp
@@ -440,7 +440,7 @@ int main( int argc, char **argv )
 
         std::map<double, particle_t>::iterator tmp;
         for (tmp = local_particles_native_map.begin(); tmp != local_particles_native_map.end(); ++tmp){
-            std::cout<<"Step, Particle ID, X, Y"<<step<<" "<<tmp->first<<" "<<(tmp->second).x>>" "<<(tmp->second).y>>std::endl;
+            std::cout<<"Step, Particle ID, X, Y"<<step<<" "<<tmp->first<<" "<<(tmp->second).x<<" "<<(tmp->second).y<<std::endl;
         }
 
 
