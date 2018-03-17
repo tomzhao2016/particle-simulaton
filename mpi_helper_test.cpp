a19dce2712f849c801f14248a5b79c2b2ca84dcb
QingyangZhao <276676925@qq.com>
2018-03-17 01:04:42 -0700

    change makefile

--- mpi_helper_test.cpp
+++ mpi_helper_test.cpp
@@ -16,28 +16,36 @@ void findLocalNeighborsTest(){
 	int cur_bin;
 	int len_row = 2;
 	int len_col = 3;
-	std::set<int> nidx; 
+	int *nidx;
+	std::set<int> nidxSet; 
 
 	cur_bin = 0;
 	nidx = {0, 1, 3, 4};
+	nidxSet = set(nidx);
 	find_local_neighbors(bin_t *bins, 0, len_row, len_col);
 	assert(ins[cur_bin].neighbor_idx  == nidx);
 
 	cur_bin = 1;
 	nidx = {0, 1, 2, 3, 4 , 5};
+	nidxSet = set(nidx);
 	find_local_neighbors(bin_t *bins, 0, len_row, len_col);
 	assert(ins[cur_bin].neighbor_idx  == nidx);
 
 	cur_bin = 2;
 	nidx = {1, 2, 4, 5};
+	nidxSet = set(nidx);
 	find_local_neighbors(bin_t *bins, 0, len_row, len_col);
 	assert(ins[cur_bin].neighbor_idx  == nidx);
 
 	cur_bin = 3;
 	nidx = {0, 1, 3, 4};
+	nidxSet = set(nidx);
 	find_local_neighbors(bin_t *bins, 0, len_row, len_col);
 	assert(ins[cur_bin].neighbor_idx  == nidx);
 
 	std::cout<<" find_local_neighbors pass tests "<<std::endl;
 
+}
+void main{
+	findLocalNeighborsTest();
 }
