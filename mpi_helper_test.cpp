151902d05ecb3fe47f439c2bc1ec66f826a9f92b
QingyangZhao <276676925@qq.com>
2018-03-17 01:09:36 -0700

    fix set typo

--- mpi_helper_test.cpp
+++ mpi_helper_test.cpp
@@ -16,36 +16,45 @@ void findLocalNeighborsTest(){
 	int cur_bin;
 	int len_row = 2;
 	int len_col = 3;
-	int *nidx;
 	std::set<int> nidxSet; 
 
 	cur_bin = 0;
-	nidx = {0, 1, 3, 4};
-	nidxSet = set(nidx);
+	nidxSet.insert(0);
+	nidxSet.insert(1);
+	nidxSet.insert(3);
+	nidxSet.insert(4);
 	find_local_neighbors(bin_t *bins, 0, len_row, len_col);
-	assert(ins[cur_bin].neighbor_idx  == nidx);
+	assert(bins[cur_bin].neighbor_idx  == nidx);
 
 	cur_bin = 1;
-	nidx = {0, 1, 2, 3, 4 , 5};
-	nidxSet = set(nidx);
+	nidxSet.insert(0);
+	nidxSet.insert(1);
+	nidxSet.insert(2);
+	nidxSet.insert(3);
+	nidxSet.insert(4);
+	nidxSet.insert(5);
 	find_local_neighbors(bin_t *bins, 0, len_row, len_col);
-	assert(ins[cur_bin].neighbor_idx  == nidx);
+	assert(bins[cur_bin].neighbor_idx  == nidx);
 
 	cur_bin = 2;
-	nidx = {1, 2, 4, 5};
-	nidxSet = set(nidx);
+	nidxSet.insert(1);
+	nidxSet.insert(2);
+	nidxSet.insert(4);
+	nidxSet.insert(5);
 	find_local_neighbors(bin_t *bins, 0, len_row, len_col);
-	assert(ins[cur_bin].neighbor_idx  == nidx);
+	assert(bins[cur_bin].neighbor_idx  == nidx);
 
 	cur_bin = 3;
-	nidx = {0, 1, 3, 4};
-	nidxSet = set(nidx);
+	nidxSet.insert(0);
+	nidxSet.insert(1);
+	nidxSet.insert(3);
+	nidxSet.insert(4);
 	find_local_neighbors(bin_t *bins, 0, len_row, len_col);
-	assert(ins[cur_bin].neighbor_idx  == nidx);
+	assert(bins[cur_bin].neighbor_idx  == nidx);
 
 	std::cout<<" find_local_neighbors pass tests "<<std::endl;
 
 }
-void main{
+void main(){
 	findLocalNeighborsTest();
 }
