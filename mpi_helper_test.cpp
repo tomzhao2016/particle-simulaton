332a50c5ce5a9aefe79340e4beea66e85668c99f
QingyangZhao <276676925@qq.com>
2018-03-19 01:15:35 -0700

    test init bins

--- mpi_helper_test.cpp
+++ mpi_helper_test.cpp
@@ -304,10 +304,10 @@ void initLocalBinsTest(){
 	int bin_len = 48;
 
 	init_local_bins(local_bins, local_particles, local_size, 
- *local_bin_size, num_proc_x, num_proc_y, rank, bin_len);
+ local_bin_size, num_proc_x, num_proc_y, rank, bin_len);
 	for (int x = 0; x< 25; x++){
 		for (int y = 0; y < 18; y++){
-			if(x == 0 || y == 0 || y = local_bin_size[1] - 1){
+			if(x == 0 || y == 0 || y == local_bin_size[1] - 1){
 				assert(local_bins[y*25+x].flag==2);
 			}
 			else if(x == 1 && y > 0 && y< 17){
